9697f1f5a2637b5044047d7bba1aa44c5a97b46d
gokhulavaasan <gokhulavaasan@gmail.com>
2024-12-20 23:05:20 +0300

    Update Constants.kt

--- app/src/main/java/com/example/news/util/Constants.kt
+++ app/src/main/java/com/example/news/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.news.util
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
-    const val API_KEY = "31a3481289e14aafa999335198b09729"
+    const val API_KEY = ""
     const val BASE_URL = "https://newsapi.org/v2/"
     const val NEWS_DATABASE_NAME = "news_db"
 }
